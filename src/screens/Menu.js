13ce021555f0502eed6ebc9a2d86f19c90a2f6e4
sonalinv <sonal@inevitableinfotech.com>
2021-12-17 15:21:03 +0530

    completed UI for food add screen

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,151 +1,265 @@
 import React, { useState, useEffect } from "react";
-import {Text, View, SafeAreaView, TextInput,  StyleSheet, TouchableOpacity,ScrollView} from "react-native";
-import {font,colors,images,customStyles,ionicons, icons, } from "../constants/styles";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Button,
+  Switch,
+} from "react-native";
+import {
+  font,
+  colors,
+  images,
+  customStyles,
+  ionicons,
+  icons,
+} from "../constants/styles";
 import { Picker } from "@react-native-picker/picker";
-import ImagePicker from 'react-native-image-crop-picker';
+import ImagePicker from "react-native-image-crop-picker";
+import Modal from "react-native-modal";
+import axios from "axios";
+import { BASE_URL } from "../constants/config";
 
 const AddNewMenu = ({ navigation }) => {
-  const [Enable, setEnable] = useState("foodcategory");
-  const [title, setTitle] = useState("");
-  const [quantity, setQuantity] = useState("");
-  const [details, setDetails] = useState("");
-  const [price, setPrice] = useState("");
+  
 
-  const foodimages = () => {
-    console.log('Choose Photo');
-    ImagePicker.openPicker({
-      width: 300,
-      height: 400,
-      cropping: true
-    }).then(image => {
-      console.log(image);
-    });
-}
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [foodcategory, setFoodCategory] = useState();
 
- 
 
+  const [isToggleEnabled, setIsToggleEnabled] = useState(false);
+
+  const toggleSwitch = () =>
+    setIsToggleEnabled((previousState) => !previousState);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  // const foodimages = () => {
+  //   console.log("Choose Photo");
+  //   ImagePicker.openPicker({
+  //     multiple: true,
+  //   }).then((images) => {
+  //     console.log(images);
+  //   });
+  // };
+  
 
   return (
     <SafeAreaView style={customStyles.container}>
-       <ScrollView>
-     
+      <ScrollView>
         <View
           style={{
             marginTop: "6%",
           }}
         >
-         
-
           <Text style={customStyles.logoText}>Add Food Screen</Text>
 
-         <View style={{marginBottom:-8}}></View>
-          <Text style={customStyles.textInputName}>Food  Title:</Text>
+          <View style={{ marginBottom: -8 }}></View>
+          <Text style={customStyles.textInputName}>Food Title:</Text>
           {/* <View style={{marginTop:-20}}></View> */}
           <TextInput
             style={customStyles.textInputfoodadd}
             underlineColorAndroid={colors.gray}
+          //   value={data.title}
+          // onChangeText={(text) => textTitleChange(text)}
             
           />
-
 
           <Text style={customStyles.textInputName}>Food Category</Text>
-         <View style={customStyles.pickerBorder}>
-          
-          <Picker
-          
-                  selectedValue={Enable}
-                 // style={customStyles.picker}
-                  mode={"dropdown"}
-                  onValueChange={(itemValue) => setEnable(itemValue)}
+          <View style={customStyles.pickerBorder}>
+            <Picker
+              selectedValue={foodcategory}
+              // style={customStyles.picker}
+              mode={"dropdown"}
+              onValueChange={(foodcategory) => setFoodCategory(foodcategory)}
+            >
+              <Picker.Item label="food category" value="foodcategory" />
+              <Picker.Item label="Chinese" value="chinese" />
+              <Picker.Item label="south indian " value="southindian" />
+            </Picker>
+          </View>
+
+          <View style={customStyles.addcategory}>
+            <TouchableOpacity onPress={toggleModal}>
+              <Text
+                style={{ fontSize: 20, borderRadius: 1, borderColor: "gray" }}
+              >
+                +
+              </Text>
+              <View>
+                <Modal
+                  style={customStyles.modal}
+                  isVisible={isModalVisible}
+                  transparent={true}
                 >
-                  <Picker.Item  label="food category" value="foodcategory" />
-                  <Picker.Item label="Chinese" value="chinese" />
-                  <Picker.Item label="south indian " value="southindian" />
-                </Picker>
-                </View>
+                  
+                  <View>
+                    <View>
+                      <Text
+                        style={{
+                          color: "black",
+                          marginLeft: -20,
+                          marginTop: -270,
+                        }}
+                      >
+                       
+                        Add Category:
+                      </Text>
+                      <TextInput
+                        style={{
+                          borderBottomWidth: 1,
+                          marginLeft: -20,
+                          marginTop: 10,
+                          height: 40,
+                          color: "black",
+                        }}
+                      ></TextInput>
+                    </View>
+                    <TouchableOpacity
+                      onPress={toggleModal}
+                      style={{
+                        marginLeft: -100,
+                        justifyContent: "center",
+                        marginTop: -160,
+                        height: 40,
+                      }}
+                    >
+                      <Text style={customStyles.cancelbuttonaddcategoryText}>
+                        Cancel
+                      </Text>
+                    </TouchableOpacity>
+                    <View style={{ marginLeft: -84 }}>
+                      <TouchableOpacity
+                        style={{
+                          marginLeft: 190,
+                          marginTop: -40,
+                          height: 40,
+                          justifyContent: "center",
+                        }}
+                        onPress={toggleModal}
+                      >
+                        <Text style={customStyles.addcategorybuttontext}>
+                          
+                          Add Category
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </Modal>
+              </View>
+            </TouchableOpacity>
+          </View>
 
-              <View style={customStyles.addcategory}><TouchableOpacity><Text style={{fontSize:20, borderRadius:1, borderColor:'gray'}} >+</Text></TouchableOpacity></View>
-              <View style={{marginTop:10}}></View>
+          <View style={{ marginTop: 10 }}></View>
           <Text style={customStyles.textInputName}>Available Quantity</Text>
-         
+
           <TextInput
             style={customStyles.textInputfoodadd}
             underlineColorAndroid={colors.gray}
-            keyboardType="number-pad"
-            value={quantity}
-            onChangeText={(quantity) => setQuantity(quantity)}
-                    />
+           
+          //   value={data.quantity}
+          //  onChangeText={(text) => quantitychange(text)}
+          />
 
-         
           <Text style={customStyles.textInputName}>Food Details</Text>
-          <View style={{marginTop:-15}}></View>
-         <ScrollView>
-          <TextInput
-            multiline={true}
-            numberOfLines={10}
-            style={customStyles.textArea1}
-            underlineColorAndroid={colors.gray}
-            value={details}
-            onChangeText={(details) => setDetails(details)}
-            
-          />
-</ScrollView>
-         
+          <View style={{ marginTop: -15 }}></View>
+          <ScrollView>
+            <TextInput
+              multiline={true}
+              numberOfLines={10}
+              
+              style={customStyles.textArea1}
+              underlineColorAndroid={colors.gray}
+              // value={data.details}
+              // onChangeText={(text) => detailschange(text)}
+            />
+          </ScrollView>
+
           <Text style={customStyles.textInputName}>Price</Text>
-         
+
           <TextInput
             style={customStyles.textInputfoodadd}
-            underlineColorAndroid={colors.gray}
-            secureTextEntry={true}
-            value={price}
-            onChangeText={(price) => setPrice(price)}
+            underlineColorAndroid={colors.gray}   
+            keyboardType="number-pad"         
+            // value={data.price}
+            // onChangeText={(text) => pricechange(text)}
           />
-                   <View style={{alignItems:'center'}}><TouchableOpacity
-                   onPress={()=>foodimages()}>
-                   <Text style={{alignContent:'center', borderWidth:1,padding:5,borderRadius:8,backgroundColor:'#ebebeb',borderColor:'#ebebeb'}} >{ionicons.folder
-}  Upload Food Images</Text></TouchableOpacity></View>
-         <View>
-        <Text style={{ marginTop: 10, marginLeft:40 }}> Status: Saved </Text>
-      </View>
-         
-      <View style={customStyles.cancelbutton}>
-        <TouchableOpacity
-          style={customStyles.cancel}
-          onPress={() => navigation.navigate("Vendor")}
+          <View style={{ alignItems: "center" }}>
+            <TouchableOpacity onPress={() => foodimages()}>
+              <Text
+                style={{
+                  alignContent: "center",
+                  borderWidth: 1,
+                  padding: 5,
+                  borderRadius: 8,
+                  backgroundColor: "#ebebeb",
+                  borderColor: "#ebebeb",
+                }}
+              >
+                {ionicons.folder} Upload Food Images
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text style={{ marginTop: 10, marginLeft: 40 }}>
+              Status: Saved
+            </Text>
+          </View>
+
+          <View style={customStyles.cancelbutton}>
+            <TouchableOpacity
+              style={customStyles.cancel}
+              //onPress={() => cancel()}
+            >
+              <Text style={{ color: "white", fontWeight: "bold" }}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={customStyles.savebutton}>
+            <TouchableOpacity
+              style={customStyles.save}
+             // onPress={onSave}
+            >
+              <Text style={{ color: "white", fontWeight: "bold" }}>Save</Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              alignContent: "center",
+              alignItems: "center",
+              marginTop: 10,
+            }}
+          ><Text
+          style={{
+            textAlign: "center",
+            paddingTop:9,
+            marginTop: -10,
+            marginLeft: -20,
+            fontWeight: "bold",
+          }}
         >
+          Publish
+        </Text>
+            <Switch
+             style={{marginTop: -24,marginLeft:70 }}
+              trackColor={{ false: "#767577", true: "#4ebf40" }}
+              thumbColor={isToggleEnabled ? "#4ebf40" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isToggleEnabled}
+            />
+          </View>
           
-          <Text style={{ color: "white" }}>Cancel</Text>
-        </TouchableOpacity>
         </View>
-
-        <View style={customStyles.savebutton}>
-
-        <TouchableOpacity
-          style={customStyles.save}
-          onPress={() => navigation.navigate("ResetPassword")}
-        >
-          <Text style={{ color: "white" }}>Save</Text>
-          
-        </TouchableOpacity>
-        </View>
-        <View style={customStyles.publishbutton}>
-        <TouchableOpacity
-          style={customStyles.publish}
-          onPress={() => navigation.navigate("Menu")}
-        >
-          <Text style={{ color: "white" }}>Publish</Text>
-          </TouchableOpacity>
-
-         
-        </View>
-        </View>
-        </ScrollView>
+      </ScrollView>
     </SafeAreaView>
   );
-      
-
 };
 
 export default AddNewMenu;
-
-  
